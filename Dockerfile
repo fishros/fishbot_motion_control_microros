c673a7ac9b8dfdc57e22fc38b0031471229f0c98
鱼香ROS <87068644+fishros@users.noreply.github.com>
2024-03-31 12:24:29 +0800

    to(docker): add esptools

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ros:humble-ros-base-jammy
 
 
-RUN sudo apt update && apt install python3-pip python3-penv -y && echo "" >> /root/.bashrc &&  pip3 install -U PlatformIO
+RUN sudo apt update && apt install python3-pip python3-penv -y && echo "" >> /root/.bashrc &&  pip3 install -U PlatformIO esptool
 
 RUN apt update && apt install ros-humble-ros-base -y
 
--- release.sh
+++ release.sh
@@ -1,3 +1,5 @@
+export PATH=/usr/local/bin/:$PATH
+
 source /opt/ros/humble/setup.bash
 mkdir -p /github/home/.platformio/penv/bin/
 touch /github/home/.platformio/penv/bin/activate
@@ -12,7 +14,7 @@ export TVERSION=v1.0.0
 export TDATA=`date +%y%m%d`
 export BINNAME=`echo $TNAME`_$TVERSION.$TDATA.bin
 
-export boot_app0_dir="/root/.platformio/packages/framework-arduinoespressif32/tools/partitions"
+# export boot_app0_dir="/root/.platformio/packages/framework-arduinoespressif32/tools/partitions"
 esptool.py  --chip esp32 merge_bin -o bin/$BINNAME --flash_mode dio --flash_size 4MB 0x1000 .pio/build/featheresp32/bootloader.bin 0x8000 .pio/build/featheresp32/partitions.bin  0x10000 .pio/build/featheresp32/firmware.bin 
 # cp bin/$BINNAME
 echo "Build Finish bin/$BINNAME"
