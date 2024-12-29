d9ccce5d7934e57ba26ea9878a9a07c329bcca8a
鱼香ROS <87068644+fishros@users.noreply.github.com>
2024-12-29 17:26:04 +0800

    to:尝试增加对jazzy版本支持

--- release.sh
+++ release.sh
@@ -4,8 +4,8 @@ export PATH=/home/runner/.platformio/penv/bin:/usr/local/bin/:$PATH
 sed -i "s/{VERSION}/$GITHUB_REF_NAME/g" include/fishbot_config.h
 pip3 install esptool
 pio lib install # Install dependencies
-cp .config/microros/colcon.meta .pio/libdeps/featheresp32/micro_ros_platformio/metas/colcon.meta
-rm -rf .pio/libdeps/featheresp32/micro_ros_platformio/libmicroros/
+cp .config/microros/colcon.meta .pio/libdeps/fishbot_motion_control_humble/micro_ros_platformio/metas/colcon.meta
+rm -rf .pio/libdeps/fishbot_motion_control_humble/micro_ros_platformio/libmicroros/
 pio run
 
 rm -rf bin && mkdir bin
@@ -16,6 +16,6 @@ export BINNAME=`echo $TNAME`_$TVERSION.$TDATA.bin
 export ELFNAME=`echo $TNAME`_$TVERSION.$TDATA.elf
 
 # export boot_app0_dir="/root/.platformio/packages/framework-arduinoespressif32/tools/partitions"
-esptool.py  --chip esp32 merge_bin -o bin/$BINNAME --flash_mode dio --flash_size 4MB 0x1000 .pio/build/featheresp32/bootloader.bin 0x8000 .pio/build/featheresp32/partitions.bin  0x10000 .pio/build/featheresp32/firmware.bin 
-cp .pio/build/featheresp32/firmware.elf bin/$ELFNAME
+esptool.py  --chip esp32 merge_bin -o bin/$BINNAME --flash_mode dio --flash_size 4MB 0x1000 .pio/build/fishbot_motion_control_humble/bootloader.bin 0x8000 .pio/build/fishbot_motion_control_humble/partitions.bin  0x10000 .pio/build/fishbot_motion_control_humble/firmware.bin 
+cp .pio/build/fishbot_motion_control_humble/firmware.elf bin/$ELFNAME
 echo "Build Finish bin/`ls bin`"
